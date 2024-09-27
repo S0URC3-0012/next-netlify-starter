ad97f147ffb436870ad00f98db7d603e9d31bf93
S0URC3-0012 <0rang3d10@gmail.com>
2024-09-26 21:02:47 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
+</Head>
+      <main>
+        <Header title="Welcome!" />
+        <p className="description">
+          WELCOME TO... <code>MRS.BARTLETT'S GAMES!!</code>
+        </p>
+      </main>
+<Footer />
 export default function Home() {
   return (
     <div className="container">
@@ -9,14 +16,7 @@ export default function Home() {
         <title>Bartlett</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome!" />
-        <p className="description">
-          WELCOME TO... <code>MRS.BARTLETT'S GAMES!!</code>
-        </p>
-      </main>
-
+    
       <Footer />
     </div>
   )
